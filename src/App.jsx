ffd6b9f02cf8965fb10b8be9d1de56d534372343
S4j331 <sajeeltipu4903@gmail.com>
2023-11-06 19:40:40 +0500

    learns

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,15 @@
 import "./App.css";
-export default function MyPage(){
+export default function MyPage() {
   return (
     <>
-    <h1 className="main-heading">Welcome to fooooteh</h1>
+      <div className="main-heading-div">
+        <h1 className="main-heading">Welcome to fooooteh</h1>
+        <div className="sign-in-heading-div">
+          <h2 className="sign-in-heading">
+            Please enter your email and password
+          </h2>
+        </div>
+      </div>
     </>
-  )
+  );
 }
