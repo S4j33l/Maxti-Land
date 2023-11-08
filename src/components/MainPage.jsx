74388d9e4936d7a5ad1ff1ec2e81bbe9e131fecd
S4j331 <sajeeltipu4903@gmail.com>
2023-11-08 11:49:12 +0500

    yez i am cook

--- src/components/MainPage.jsx
+++ src/components/MainPage.jsx
@@ -1,7 +1,33 @@
-export default function MainPage(){
-    return (
-        <h1>
-            Welcome to fooooteh
-        </h1>
-    )
+import "./MainPage.css";
+export default function MainPage() {
+  return (
+    <>
+      <div className="welcome-main-heading-div">
+        <h1 className="welcome-main-heading">Welcome back nigga!</h1>
+        <h2 className="selection-sub-heading">Select any team!</h2>
+      </div>
+      <div className="main-content-div">
+        <p className="grid-item">
+          Lorem, ipsum dolor sit amet consectetur adipisicing elit. Harum hic
+          ab, officia, quis modi, deserunt accusamus ipsum sequi culpa quibusdam
+          illum fuga facere porro. Sed, maxime! Eum eveniet commodi accusamus?
+        </p>
+        <p className="grid-item">
+          Lorem, ipsum dolor sit amet consectetur adipisicing elit. Harum hic
+          ab, officia, quis modi, deserunt accusamus ipsum sequi culpa quibusdam
+          illum fuga facere porro. Sed, maxime! Eum eveniet commodi accusamus?
+        </p>
+        <p className="grid-item">
+          Lorem, ipsum dolor sit amet consectetur adipisicing elit. Harum hic
+          ab, officia, quis modi, deserunt accusamus ipsum sequi culpa quibusdam
+          illum fuga facere porro. Sed, maxime! Eum eveniet commodi accusamus?
+        </p>
+        <p className="grid-item">
+          Lorem, ipsum dolor sit amet consectetur adipisicing elit. Harum hic
+          ab, officia, quis modi, deserunt accusamus ipsum sequi culpa quibusdam
+          illum fuga facere porro. Sed, maxime! Eum eveniet commodi accusamus?
+        </p>
+      </div>
+    </>
+  );
 }
